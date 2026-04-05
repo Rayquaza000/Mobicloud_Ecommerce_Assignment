a1e5ad5defabf1c2a586487a044c77ee4100c8c6
Rayquaza000 <mehtapratik2001@gmail.com>
2026-04-05 15:44:50 +0530

    Started the project

---
+++ Backend/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose.connect(process.env.DATABASE_URL);
+const db=mongoose.connection;
+
+db.on("open",()=>{
+    console.log("Database connection successful")
+})
+
+db.on("error",()=>{
+    console.log("Database connection error")
+})
