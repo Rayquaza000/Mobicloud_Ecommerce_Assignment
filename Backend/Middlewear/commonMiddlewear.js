e3fdef190c8bd3bbb131dbe614cd59b34d992e13
Rayquaza000 <mehtapratik2001@gmail.com>
2026-04-09 21:35:36 +0530

    still fixing bugs.

--- Backend/Middlewear/commonMiddlewear.js
+++ Backend/Middlewear/commonMiddlewear.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 
 export function verifyJWT(req, res, next) {
     try {
-        if(jwt.verify(req.cookies.token, process.env.JWT_SECRET_KEY));
+        if(jwt.verify(req.cookies.token, process.env.JWT_SECRET_KEY))
         {
             console.log("Reached verification")
         next();
