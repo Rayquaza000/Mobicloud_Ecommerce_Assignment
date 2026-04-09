55e919d9eea7454204ea15a188c965edadb41e7e
Rayquaza000 <mehtapratik2001@gmail.com>
2026-04-09 22:43:39 +0530

    still fixing bugs.

--- Client/src/MainLayout.jsx
+++ Client/src/MainLayout.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import Header from './components/Header.jsx'
 import { Outlet } from 'react-router-dom'
-
+import {useNavigate} from "react-router-dom"
 const MainLayout = () => {
+  const navigate=useNavigate();
   return (
     <div className="bg-[#B9CDE5] flex flex-col min-h-screen">
         <Header/>
         <hr className='border border-white'></hr>
         <div>
-            <span>/Home</span>
+            <span className="text-20 ml-4 underline" onClick={()=>{navigate("/")}}>Home</span>
         </div>
         <hr className='border border-white'></hr>
         <Outlet />
